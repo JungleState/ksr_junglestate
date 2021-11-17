fb523754b3a264c1239784c76ff1eee1dbf3f80e
PhilippDoenni <philipp.doenni@gmail.com>
2021-11-17 16:46:45 +0100

    this happened through an automated process, I have no idea

--- static/data/view.js
+++ static/data/view.js
@@ -13,26 +13,28 @@ class View{
         }
     }
     showField(type, matrix){ //show the field
-        if (type=='spectator'){ //Dimensions for spectator
-            let width= 50; 
-            let height=50;
+        let width;
+        let height;
+        if (type == 'spectator'){ //Dimensions for spectator
+            width = 50; 
+            height =50;
         }
-        if (type=='client'){ //Dimensions for human player
-            let widht= 5;
-            let height=5;
+        if (type == 'client'){ //Dimensions for human player
+            widht = 5;
+            height = 5;
         }
-        let row=0
+        let row = 0
         for (let row_element of this.field.getElementsByClassName("row")) { //needs Rows in HTML (so I get Rows)
-            row+=1
-            let column=0
+            row += 1
+            let column = 0
             for(let tile of row_element.getElementsByTagName("tile")) { //Every Tile in each Row needs to be called "tile"
-                column+=1
+                column += 1
                 let tiletype = matrix[row][column]
 
-                if(tiletype==0){ //probably solve this with dicts (MORE BEAUTIFUL)
+                if(tiletype == 0){ //probably solve this with dicts (MORE BEAUTIFUL)
                     tile.classList.add('plain')
                 }
-                if(tiletype==1){
+                if(tiletype == 1){
                     tile.classList.add('jungle')
                 }
 
