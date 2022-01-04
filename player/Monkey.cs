62bae4212fed5bf573947ddb1fa4690bab79a440
PhilippDoenni <philipp.doenni@gmail.com>
2022-01-04 16:01:14 +0100

    this happened through an automated process, I have no idea

---
+++ player/Monkey.cs
@@ -0,0 +1,44 @@
+﻿using Program;
+using System;
+
+// For usage in Visual Studio Code:
+//     - Download C# extension
+//     - Download NET: https://dotnet.microsoft.com/download
+//     - Open Terminal
+//     - Move into "player" folder: "cd player"
+//     - Download Json.NET: "dotnet add package Newtonsoft.Json
+//     - Run: "dotnet run"
+
+namespace junglestate {
+    class Monkey : BaseMonkey {
+        Monkey() {
+            super("Max Mustermann");
+        }
+        public override Move nextMove(GameState state) {
+            Direction direction = selectRandomDirection(state);
+            return new Action{action = Action.MOVE, direction = direction, nextRound = state.round};
+        }
+
+        private Direction selectRandomDirection(GameState state) {
+            List<Direction> freeDirs = computeFreeDirections(state);
+            Random random = new System.Random();
+            int r = random.NextInt(freeDirs.Size());
+            return freeDirs[r];
+        }
+
+        private List<Direction> computeFreeDirs(GameState state) {
+            List<Direction> results = new List<>();
+            foreach (Direction dir in Enum.GetValues(typeof(Direction))) {
+                Cell cell = dir.GetCell(state);
+                if (cell.isMoveable()) {
+                    result.Add(dir);
+                }
+            }
+            return result;
+        }
+
+        static async Task Main(string[] args) {
+            ProgramMain(args, new Monkey());
+        }
+    }
+}
--- player/Program.cs
+++ player/Program.cs
@@ -1,16 +1,8 @@
-﻿using System;
-using Newtonsoft.Json;
+using System;
+using Newtonsof.Json;
 
-// For usage in Visual Studio Code:
-//     - Download C# extension
-//     - Download NET: https://dotnet.microsoft.com/download
-//     - Open Terminal
-//     - Move into "player" folder: "cd player"
-//     - Download Json.NET: "dotnet add package Newtonsoft.Json
-//     - Run: "dotnet run"
-
-namespace Player {
-    class Program {
+namespace junglestate {
+        class Program {
         private static readonly HttpClient client = new HttpClient();
 
         private static async Task joinGame(Tuple<string?, string?> configs) {
@@ -123,128 +115,64 @@ namespace Player {
             }
         }
 
-        private static Tuple<string, string, bool> loadConfigs() {
-            // configurations that need to be changed by the user
+        private static Tuple<string, string, bool> loadConfigs(Monkey monkey) {
+            // configurations
 
-//vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv//
-//vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv/YOUR/CONFIGS/BELOW/HERE/vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv//
-//vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv//
+            // url of server
+            // e.g. http://localhost:5500/
+            string url = "http://localhost:5500/";
 
+            // name of player
+            // e.g. Max Mustermann
+            string name = monkey.name;
 
+            // debug mode
+            // app mode requires you to enter url and name after starting app
+            bool appMode = true;
 
-
-
-// url of server
-// e.g. http://localhost:5500/
-string url = "http://localhost:5500/";
-
-// name of player
-// e.g. Max Mustermann
-string name = "Max Mustermann";
-
-// debug mode
-// app mode requires you to enter url and name after starting app
-bool appMode = true;
-
-
-
-
-
-//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^//
-//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^/YOUR/CONFIGS/ABOVE/HERE/^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^//
-//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^//
             Tuple<string, string, bool> configs = new Tuple<string, string, bool>(url, name, appMode);
             return configs;
         }
 
         private static void playerBehaviour(Tuple<string, string> c, string field, int health, int ammo, int score, int round) {   
-        // method with the user written algorithm
-                
-//vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv//
-//vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv/YOUR/CODE/BELOW/HERE/vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv//
-//vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv//
+        // get user written algorithm
 
-// attack:  attack(c, DIRECTION)   options for DIRECTION: [0, 7]
-// move:    move(c, DIRECTION)     options for DIRECTION: -1, 0, 2, 4, 6
-// DIRECTION: integer
-//    -1: No direction
-//    0: up
-//    1: up right
-//    2: right
-//    3: down right
-//    4: down
-//    5: down left
-//    6: left
-//    7: up left
+        // attack:  attack(c, DIRECTION)   options for DIRECTION: [0, 7]
+        // move:    move(c, DIRECTION)     options for DIRECTION: -1, 0, 2, 4, 6
+        // DIRECTION: integer
+        //    -1: No direction
+        //    0: up
+        //    1: up right
+        //    2: right
+        //    3: down right
+        //    4: down
+        //    5: down left
+        //    6: left
+        //    7: up left
 
-// "  ": plain:     empty
-// "FF": jungle:    wall
-// "CC": coconut:   +1 ammo
-// "BB": banana:    +1 health
-// "PP": pineapple: +1 score
+        // "  ": plain:     empty
+        // "FF": jungle:    wall
+        // "CC": coconut:   +1 ammo
+        // "BB": banana:    +1 health
+        // "PP": pineapple: +1 score
 
-// string field: 5x5 matrix of surrounding
-// int health: health of character
-// int ammo: amount of ammo left
-// int score: score of player
-// int round: round of the game
-// Tuple<string, string> c: configurations, can be ignored but must be given as an argument for attack() and move()
+        // string field: 5x5 matrix of surrounding
+        // int health: health of character
+        // int ammo: amount of ammo left
+        // int score: score of player
+        // int round: round of the game
+        // Tuple<string, string> c: configurations, can be ignored but must be given as an argument for attack() and move()
 
-// custom mehtods can be added in the next section.
-// DON'T DO IT HERE!
+        // custom mehtods can be added in the next section.
+        // DON'T DO IT HERE!
 
 
-
-
-
-Random random = new Random();
-int decision = random.Next(0, 2);
-
-if (decision == 0){
-    move(c, -1);
-}
-else {
-    moveLeft(c);
-}
-
-
-
-
-
-//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^//
-//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^/YOUR/CODE/ABOVE/HERE/^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^//
-//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^//
 
         }
 
-//vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv//
-//vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv/YOUR/METHODS/BELOW/HERE//vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv//
-//vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv//
-
-// space for some custom methods
-// if attack() or move() methods are used, you need to give the "c" as an argument
-// e.g. private static void moveLeft(Tuple<string, string> c) {...}
-
-
-
-
-
-private static void moveLeft(Tuple<string, string> c) {
-    move(c, 6);
-}
-
-
-
-
-
-//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^//
-//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^/YOUR/METHODS/ABOVE/HERE/^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^//
-//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^//
-
-
-        static async Task Main(string[] args) {
+        static async Task ProgramMain(string[] args, Monkey monkey) {
             // start the process
-            Tuple<string, string, bool> rawConfigs = loadConfigs();
+            Tuple<string, string, bool> rawConfigs = loadConfigs(monkey);
             Console.Clear();
             if (rawConfigs.Item3 == false) {
                 // normal start
