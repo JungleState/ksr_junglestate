96cfda50cdd98136d5d1cc7462d8253170f9dba6
Sheran Keller <shkelle1@ksr.ch>
2022-01-04 16:04:58 +0100

    multi-server

--- server/static/login.js
+++ server/static/login.js
@@ -33,10 +33,32 @@ async function login(loginMode) {
 
     document.getElementById('name').value = "";
     document.getElementById('mode').checked = false;
+
+}
+
+function refresh(json){
+    parentElement = document.getElementById('openGames');
+    json.games.forEach((item, index) => {
+        console.log(`${index} : ${item}`);
+        var div = document.createElement('div'); 
+        div.textContent = "Server " + index;  
+        parentElement.appendChild(div)  
+    });
 }
 
 setInterval(async () => {
     let response = await fetch('/getGames');
     let json = await response.json();
     console.log(json);
+
+    parentElement = document.getElementById('openGames');
+
+    while (parentElement.hasChildNodes()) {  
+        parentElement.removeChild(parentElement.firstChild);
+    } 
+    
+    var div = document.createElement('div'); 
+    div.textContent = "Server:    " + json.games.id;  
+    parentElement.appendChild(div)  
+    //json.games.forEach(element => console.log(element));
 }, 1000);
