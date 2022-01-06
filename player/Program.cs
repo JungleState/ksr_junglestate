f507c30e28b2e56f3f6a628cec012b3fe16c7d72
Tom Hofmann <tom@scheidweg.net>
2022-01-06 15:15:57 +0100

    use Program members instead of params

--- player/Program.cs
+++ player/Program.cs
@@ -1,13 +1,22 @@
 using Newtonsoft.Json;
 
 namespace junglestate {
+    public sealed class JungleConfig {
+        public string serverAddress = "http://localhost:5500/";
+    }
+
+    ///<summary>The main program for junglecamp monkey bots.</summary>
+    ///<remarks>Once a program is started, it maintains a HTTP connection to the game server
+    ///  listens for state updates and calls the monkey's <see cref="BaseMonkey.nextMove(GameState)"/>
+    ///  method determine the monkey's behavior.
+    ///</remarks>
     class Program {
         private readonly HttpClient client;
-        private readonly Tuple<string?, string?> configs;
+        private readonly JungleConfig config;
         private readonly BaseMonkey monkey;
 
-        private Program(BaseMonkey monkey, Tuple<string?, string?> configs) {
-            this.configs = configs;
+        private Program(BaseMonkey monkey, JungleConfig config) {
+            this.config = config;
             this.monkey = monkey;
             this.client = new HttpClient();
         }
@@ -17,7 +26,7 @@ namespace junglestate {
             int SLEEP_TIME = 50;
 
             // join game (fetch request)
-            var response = await client.PostAsync(configs.Item1+"joinGame/client/"+configs.Item2, new StringContent(""));
+            var response = await client.PostAsync(config.serverAddress+"joinGame/client/"+monkey.name, new StringContent(""));
             var json = await response.Content.ReadAsStringAsync();
             dynamic? data = JsonConvert.DeserializeObject(json);
 
@@ -31,7 +40,7 @@ namespace junglestate {
                     int score = 0;
                     // loop with regular request for the field followed by an instruction for the server what to do
                     while (true) {
-                        Tuple<bool, int> returnedData = await getData(monkey, configs);
+                        Tuple<bool, int> returnedData = await getData();
                         Thread.Sleep(SLEEP_TIME);
                         if (returnedData.Item1) {
                             score = returnedData.Item2;
@@ -49,9 +58,9 @@ namespace junglestate {
                 Console.WriteLine("ERROR: data is null");
             }
         }
-        private async Task<Tuple<bool, int>> getData(BaseMonkey monkey, Tuple<string?, string?> configs) {
+        private async Task<Tuple<bool, int>> getData() {
             // get the map
-            var stringTask = client.GetStringAsync(configs.Item1+"view");
+            var stringTask = client.GetStringAsync(config.serverAddress+"view");
             var json = await stringTask;
             dynamic? data = JsonConvert.DeserializeObject(json);
 
@@ -67,7 +76,7 @@ namespace junglestate {
                     Console.WriteLine("Health: "+data.lives);
                     Console.WriteLine("Ammo: "+data.coconuts);
                     Console.WriteLine("Score: "+data.points);
-                    playerBehaviour(monkey, configs, data.field, data.lives.ToObject<int>(), data.coconuts.ToObject<int>(), data.points.ToObject<int>(), data.round.ToObject<int>());
+                    playerBehaviour(data.field, data.lives.ToObject<int>(), data.coconuts.ToObject<int>(), data.points.ToObject<int>(), data.round.ToObject<int>());
                 }
                 else {
                     // end loop from joinGame
@@ -80,34 +89,34 @@ namespace junglestate {
             return new Tuple<bool, int>(gameOver, score);
         }
 
-        private void move(Tuple<string, string> configs, int direction) {
+        private void move(int direction) {
             // option for user written algorithm
             // direction defines the direction the player moves
             if (direction == 0 || direction == 2 || direction == 4 || direction == 6) {
-                sendCommand(configs, 1, direction);
+                sendCommand(1, direction);
             }
             // if an invalid direction is given or -1 is stated the character does nothing
             else {
-                sendCommand(configs, 0, -1);
+                sendCommand(0, -1);
             }
         }
 
-        private void attack(Tuple<string, string> configs, int direction) {
+        private void attack(int direction) {
             // option for user written algorithm
             // direction defines the direction the player shoots
             if (direction >= 0 && direction <= 7) {
-                sendCommand(configs, 2, direction);
+                sendCommand(2, direction);
             }
             // if an invalid direction is given the character does nothing
             else {
-                sendCommand(configs, 0, -1);
+                sendCommand(0, -1);
             }
         }
 
-        private async void sendCommand(Tuple<string, string> configs, int type, int direction) {
+        private async void sendCommand(int type, int direction) {
             // send the chosen action to the server
             try {
-                var response = await client.PostAsync(configs.Item1+"action/"+type+"/"+direction, new StringContent(""));
+                var response = await client.PostAsync(config.serverAddress+"action/"+type+"/"+direction, new StringContent(""));
                 
                 string[] actionsArray = new string[] {"stay", "move", "attack"};
                 string[] directionsArray = new string[] {"up", "up right", "right", "down right", "down", "down left", "left", "up left", "on the spot"};
@@ -122,26 +131,7 @@ namespace junglestate {
             }
         }
 
-        private static Tuple<string, string, bool> loadConfigs(Monkey monkey) {
-            // configurations
-
-            // url of server
-            // e.g. http://localhost:5500/
-            string url = "http://localhost:5500/";
-
-            // name of player
-            // e.g. Max Mustermann
-            string name = monkey.name;
-
-            // debug mode
-            // app mode requires you to enter url and name after starting app
-            bool appMode = true;
-
-            Tuple<string, string, bool> configs = new Tuple<string, string, bool>(url, name, appMode);
-            return configs;
-        }
-
-        private void playerBehaviour(BaseMonkey monkey, Tuple<string, string> c, string[] field, int health, int ammo, int score, int round) {   
+        private void playerBehaviour(string[] field, int health, int ammo, int score, int round) {   
         // get user written algorithm
         // attack:  attack(c, DIRECTION)   options for DIRECTION: [0, 7]
         // move:    move(c, DIRECTION)     options for DIRECTION: -1, 0, 2, 4, 6
@@ -167,18 +157,16 @@ namespace junglestate {
         // int ammo: amount of ammo left
         // int score: score of player
         // int round: round of the game
-        // Tuple<string, string> c: configurations, can be ignored but must be given as an argument for attack() and move()
-
             Move next = monkey.nextMove(new GameState(getCells(field), round, new PlayerInfo(monkey.name, health, ammo, score)));
             switch(next.action) {
                 case Action.MOVE:
-                    move(c, (int)next.direction);
+                    move((int)next.direction);
                     break;
                 case Action.THROW:
-                    attack(c, (int)next.direction);
+                    attack((int)next.direction);
                     break;
                 default:
-                    move(c, (int)Action.STAY);
+                    move((int)Action.STAY);
                     break;
             }
         }
@@ -209,37 +197,25 @@ namespace junglestate {
 
         public static async Task ProgramMain(string[] args, Monkey monkey) {
             // start the process
-            Tuple<string, string, bool> rawConfigs = loadConfigs(monkey);
-            Tuple<string?, string?> configs;
+            JungleConfig config = new JungleConfig();
 
-            if (rawConfigs.Item3 == false) {
-                // normal start
-                configs = new Tuple<string?, string?>(rawConfigs.Item1, rawConfigs.Item2);
-            }
-            else {
-                // app mode start
+            if (true) {
+                // FIXME make CLI dependent on flags
                 try {
                     // ask url
                     Console.WriteLine("Enter url of server");
                     Console.WriteLine("e.g. http://localhost:5500/");
                     string? url = Console.ReadLine();
-
-                    // ask name
-                    Console.Clear();
-                    Console.WriteLine("Enter name of player");
-                    string? name = Console.ReadLine();
-
-                    // start
-                    Console.Clear();
-                    configs = new Tuple<string?, string?>(url, name);
-                }
-                catch {
+                    if (url != null) {
+                        config.serverAddress = url;
+                    }
+                } catch {
                     Console.WriteLine("Invalid input.");
                     return;
                 }
             }
             Console.Clear();
-            Program program = new Program(monkey, configs);
+            Program program = new Program(monkey, config);
             await program.joinGame();
         }
     }
