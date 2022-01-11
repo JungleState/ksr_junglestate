b5451974c8706cb6cbe3c9236b9013d52f3f07b3
Tom Hofmann <tom@scheidweg.net>
2022-01-11 14:59:36 +0100

    replace block namespace by namespace decl

--- player/Monkey.cs
+++ player/Monkey.cs
@@ -1,26 +1,26 @@
-﻿namespace junglestate {
-    ///<summary>A simple monkey that moves randomly in free directions.</summary>
-    public class Monkey : BaseMonkey {
-        public override Move nextMove(GameState state) {
-            Direction direction = selectRandomDirection(state);
-            return new Move(Action.MOVE, direction, state.round);
-        }
+﻿namespace junglestate;
 
-        private Direction selectRandomDirection(GameState state) {
-            List<Direction> freeDirs = computeFreeDirections(state);
-            Random random = new System.Random();
-            Random r = new Random();
-            return freeDirs[r.Next(freeDirs.Count)];
-        }
+///<summary>A simple monkey that moves randomly in free directions.</summary>
+public class Monkey : BaseMonkey {
+    public override Move nextMove(GameState state) {
+        Direction direction = selectRandomDirection(state);
+        return new Move(Action.MOVE, direction, state.round);
+    }
 
-        private List<Direction> computeFreeDirections(GameState state) {
-            List<Direction> result = new List<Direction>();
-            foreach (Direction dir in Enum.GetValues(typeof(Direction))) {
-                if (dir.isMoveable() && state.getCell(dir).isFree()) {
-                    result.Add(dir);
-                }
+    private Direction selectRandomDirection(GameState state) {
+        List<Direction> freeDirs = computeFreeDirections(state);
+        Random random = new System.Random();
+        Random r = new Random();
+        return freeDirs[r.Next(freeDirs.Count)];
+    }
+
+    private List<Direction> computeFreeDirections(GameState state) {
+        List<Direction> result = new List<Direction>();
+        foreach (Direction dir in Enum.GetValues(typeof(Direction))) {
+            if (dir.isMoveable() && state.getCell(dir).isFree()) {
+                result.Add(dir);
             }
-            return result;
         }
+        return result;
     }
 }
