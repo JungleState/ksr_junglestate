4d097075d46151e6abc1a113149ed636abe854c0
Einstein1337 <pacifico4@gmx.ch>
2022-01-18 14:46:19 +0100

    Min bot funktioniert vilicht

--- player/Monkey.cs
+++ player/Monkey.cs
@@ -1,33 +1,85 @@
 ﻿namespace junglestate;
 
 ///<summary>A simple monkey that moves randomly in free directions.</summary>
-public class Monkey : BaseMonkey {
+public class Monkey : BaseMonkey 
+{
     private Direction lastDir = Direction.NONE;
-    public override Move nextMove(GameState state) {
-        // Attempt to move in the same direction as last round.
-        if (lastDir.isMoveable() && state.getCell(lastDir).isFree()) {
-            return new Move(Action.MOVE, lastDir, state.round, "it's so boring...");
+    public override Move nextMove(GameState state) 
+    {
+        if (state.playerInfo.coconuts > 0)
+        {
+            return new Move(Action.THROW, Direction.UP, state.round, "I bin khul...");
         }
-        // Otherwise: random move
-        Direction direction = selectRandomDirection(state);
-        lastDir = direction;
-        return new Move(Action.MOVE, direction, state.round, "didn't see this coming, eh?");
+        List<int> coords = getCoordOfNextItem(state);
+        Action action = Action.MOVE;
+        Direction dir = Direction.UP;
+        int x = coords[0];
+        int y = coords[1];
+        if (x == -1)
+        {
+            if (lastDir.isMoveable() && state.getCell(lastDir).isFree()) 
+            {
+                return new Move(Action.MOVE, lastDir, state.round, "I suäch ässä...");
+            }
+            Direction direction = selectRandomDirection(state);
+            lastDir = direction;
+            return new Move(Action.MOVE, direction, state.round, "I suäch ässä...");
+        }
+        if (x < 2)
+        {
+            dir = Direction.LEFT;
+        }
+        else if (x > 2)
+        {
+            dir = Direction.RIGHT;
+        }
+        else if (y < 2)
+        {
+            dir = Direction.UP;
+        }
+        else if (x > 2)
+        {
+            dir = Direction.DOWN;
+        }
+        return new Move(action, dir, state.round, "I han äs Item gfundä...");
     }
 
-    private Direction selectRandomDirection(GameState state) {
+    private List<int> getCoordOfNextItem(GameState state)
+    {  
+        for (int row = 0; row < 5; row++)
+        {
+            for (int column = 0; column < 5; column++)
+            {   
+                switch (state.cells[row][column].item)
+                {
+                    case Item.BANANA:
+                    case Item.PINEAPPLE:
+                        return new List<int> {row, column};
+                }
+            }
+        }
+        return new List<int> {-1, -1};
+    }
+
+    private Direction selectRandomDirection(GameState state) 
+    {
         List<Direction> freeDirs = computeFreeDirections(state);
         Random random = new System.Random();
         Random r = new Random();
         return freeDirs[r.Next(freeDirs.Count)];
     }
 
-    private List<Direction> computeFreeDirections(GameState state) {
+    private List<Direction> computeFreeDirections(GameState state) 
+    {
         List<Direction> result = new List<Direction>();
-        foreach (Direction dir in Enum.GetValues(typeof(Direction))) {
-            if (dir.isMoveable() && state.getCell(dir).isFree()) {
+        foreach (Direction dir in Enum.GetValues(typeof(Direction))) 
+        {
+            if (dir.isMoveable() && state.getCell(dir).isFree()) 
+            {
                 result.Add(dir);
             }
         }
         return result;
     }
 }
+
