d48a53310561836461405aeeaad864999f6c30ba
MaTejjprogramming <matthias.bn03@gmail.com>
2022-01-19 17:21:21 +0100

    update

--- player/Monkey.cs
+++ player/Monkey.cs
@@ -5,8 +5,6 @@ public class Monkey : BaseMonkey {
 
     private Direction defaultDir = Direction.RIGHT;
     private string monkeyMode = "walk";
-    private bool chasingItem = false;
-    private bool attack = false;
 
     public override Move nextMove(GameState state) {
         List<Direction> freeDirs = computeFreeDirections(state);
@@ -18,26 +16,25 @@ public class Monkey : BaseMonkey {
         if (nextDir == Direction.NONE || state.getCell(nextDir).item == Item.FOREST) {
             if (this.monkeyMode == "walk") {
                 if (freeDirs.Contains(this.defaultDir)) {
-                    nextDir = this.defaultDir;
                     if (r.Next(5) == 0) {
                         // 25% chance to change direction
                         nextDir = newRandomDirection(freeDirs, r);
                     }
+                    nextDir = this.defaultDir;
                 }
                 else {
                     nextDir = newRandomDirection(freeDirs, r);
                 }
 
-                return new Move(Action.MOVE, nextDir, state.round, "Food?");
+                return new Move(Action.MOVE, nextDir, state.round, "Just walking...");
             }
         }
         else {
-            System.Console.WriteLine("Run for food");
-            if (this.attack) {
-                return new Move(Action.THROW, nextDir, state.round, "FOOOD");
+            if (this.monkeyMode == "attack") {
+                return new Move(Action.THROW, nextDir, state.round, "Attack!!");
             }
             else {
-                return new Move(Action.MOVE, nextDir, state.round, "FOOOD");
+                return new Move(Action.MOVE, nextDir, state.round, "Yummy");
             }
         }
 
@@ -57,21 +54,32 @@ public class Monkey : BaseMonkey {
         return nextDir;
     }
 
+    private bool decideMove(GameState state, Item item) {
+        if (item == Item.PLAYER) { // Attack Mode
+            if (state.playerInfo.lives == 1 || state.playerInfo.coconuts == 0) {
+                return false;
+            }
+        }
+        else if (item == Item.BANANA) {
+            if (state.playerInfo.lives == 3) {
+                return false;
+            } 
+        }
+        else if (item == Item.COCONUT) {
+            if (state.playerInfo.coconuts == 3) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     private Direction getItem(string type, GameState state, List<Direction> freeDirs, Random r) {
-        List<Item> food = new List<Item> {Item.BANANA, Item.COCONUT, Item.PINEAPPLE};
         List<(int, int)> coords = new List<(int, int)>();
         Direction newDir = Direction.NONE;
         for (int i = 0; i < state.cells.Length; i++) {
             for (int j = 0; j < state.cells[i].Length; j++) {
-                if (food.Contains(state.cells[i][j].item)) {
-                    if (state.cells[i][j].item == Item.COCONUT && state.playerInfo.coconuts < 3) {
-                        coords.Add((i, j));
-                    }
-                    else if (state.cells[i][j].item == Item.PINEAPPLE || state.cells[i][j].item == Item.BANANA) {
-                        coords.Add((i, j));
-                    }
-                }
-                else if (state.cells[i][j].item == Item.PLAYER) {
+                if (decideMove(state, state.cells[i][j].item)) {
                     coords.Add((i, j));
                 }
             }
@@ -81,58 +89,53 @@ public class Monkey : BaseMonkey {
             if (dir.Item2 == 2) {
                 if (dir.Item1 < 2) {
                     if (state.cells[dir.Item1][dir.Item2].item == Item.PLAYER) {
-                        attack = true;
+                        this.monkeyMode = "attack";
                     }
                     else {
-                        attack = false;
+                        this.monkeyMode = "walk";
                     }
                     System.Console.WriteLine("Item on top");
                     newDir = Direction.UP;
-                    this.chasingItem = true;
                     return;
                 }
                 else if (dir.Item1 > 2) {
                     if (state.cells[dir.Item1][dir.Item2].item == Item.PLAYER) {
-                        attack = true;
+                        this.monkeyMode = "attack";
                     }
                     else {
-                        attack = false;
+                        this.monkeyMode = "walk";
                     }
                     System.Console.WriteLine("Item on bottom");
                     newDir = Direction.DOWN;
-                    this.chasingItem = true;
                     return;
                 }
             }
             else if (dir.Item2 < 2 || dir.Item2 > 2) {
                 if (dir.Item1 == 2 && dir.Item2 < 2) {
                     if (state.cells[dir.Item1][dir.Item2].item == Item.PLAYER) {
-                        attack = true;
+                        this.monkeyMode = "attack";
                     }
                     else {
-                        attack = false;
+                        this.monkeyMode = "walk";
                     }
                     System.Console.WriteLine("Item left");
                     newDir = Direction.LEFT;
-                    this.chasingItem = true;
                     return;
                 }
                 else if (dir.Item1 == 2 && dir.Item2 > 2) {
                     if (state.cells[dir.Item1][dir.Item2].item == Item.PLAYER) {
-                        attack = true;
+                        this.monkeyMode = "attack";
                     }
                     else {
-                        attack = false;
+                        this.monkeyMode = "walk";
                     }
                     System.Console.WriteLine("Item right");
                     newDir = Direction.RIGHT;
-                    this.chasingItem = true;
                     return;
                 }
             }
 
             if (dir.Item1 == 2 && dir.Item2 == 2) {
-                this.chasingItem = false;
                 return;
             }
         }); 
