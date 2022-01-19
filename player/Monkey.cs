864f1ca6c921badadfabd495d770494b5dd9b94e
PhilippDoenni <philipp.doenni@gmail.com>
2022-01-19 16:51:04 +0100

    can now collect items on adjacent field

--- player/Monkey.cs
+++ player/Monkey.cs
@@ -3,18 +3,22 @@
 ///<summary>A simple monkey that moves randomly in free directions.</summary>
 public class Monkey : BaseMonkey {
     private Direction lastDir = Direction.NONE;
+    private int MAX_LIVES = 3;
+    private int MAX_AMMO = 3;
+
     public override Move nextMove(GameState state) {
         // Attack player if possible
         if (state.playerInfo.coconuts > 0) {
-            Direction target = DirectionOfEnemyInRangeWithLowestHealth(state);
+            Direction target = directionOfEnemyInRangeWithLowestHealth(state);
             if (target != Direction.NONE) {
                 return new Move(Action.THROW, target, state.round, "RATATATATA!");
             }
         }
 
         // Collect items
-        if (DirectionOfClosestItemOfHighestValue(state) != Direction.NONE) {
-            return new Move(Action.MOVE, DirectionOfEnemyInRangeWithLowestHealth(state), state.round, "Items. Items! ITEMS!!");
+        Direction directionOfItem = directionOfClosestItemOfHighestValue(state);
+        if (directionOfItem != Direction.NONE) {
+            return new Move(Action.MOVE, directionOfItem, state.round, "Items. Items! ITEMS!!");
         }
 
         // Otherwise: attempt to move in the same direction as last round
@@ -47,7 +51,7 @@ public class Monkey : BaseMonkey {
         return result;
     }
 
-    private Direction DirectionOfEnemyInRangeWithLowestHealth(GameState state) {
+    private Direction directionOfEnemyInRangeWithLowestHealth(GameState state) {
         // returns direction of enemy in range with lowest health
         Direction target = Direction.NONE;
         foreach (Direction dir in Enum.GetValues(typeof(Direction))) {
@@ -60,22 +64,70 @@ public class Monkey : BaseMonkey {
         return target;
     }
 
-    private Direction DirectionOfClosestItemOfHighestValue(GameState state) {
+    private Direction directionOfClosestItemOfHighestValue(GameState state) {
         // returns direction of collectable item that is used most
         Direction target = Direction.NONE;
-        // look at closest items
+        // item can be reached in 1 turn
         foreach (Direction dir in Enum.GetValues(typeof(Direction))) {
             if (dir.isMoveable() && state.getCell(dir).item == Item.PINEAPPLE) {
                 return dir;
             }
-            else if (dir.isMoveable() && state.getCell(dir).item == Item.BANANA && state.playerInfo.lives < 3) {
+            if (dir.isMoveable() && state.getCell(dir).item == Item.BANANA && state.playerInfo.lives < MAX_LIVES) {
+                return dir;
+            }
+            if (dir.isMoveable() && state.getCell(dir).item == Item.COCONUT && state.playerInfo.coconuts < MAX_AMMO) {
                 return dir;
             }
         }
+        // item can be reached in 2 turns
+        
+
+
+
+
+
         return target;
     }
 
-    private Item compareTwoItems(Item item0, Item item1) {
-        return item0;
+    private Item compareTwoItems(GameState state, Item item0, Item item1) {
+        // returns the item that is needed most
+        int item0Value = 0;
+        int item1Value = 0;
+        switch(item0) {
+            case Item.PINEAPPLE:
+                item0Value = 3;
+                break;
+            case Item.BANANA:
+                if (state.playerInfo.lives < MAX_LIVES) {
+                    item0Value = 2;
+                }
+                break;
+            case Item.COCONUT:
+                if (state.playerInfo.coconuts < MAX_AMMO) {
+                    item0Value = 1;
+                }
+                break;
+        }
+        switch(item1) {
+            case Item.PINEAPPLE:
+                item1Value = 3;
+                break;
+            case Item.BANANA:
+                if (state.playerInfo.lives < MAX_LIVES) {
+                    item1Value = 2;
+                }
+                break;
+            case Item.COCONUT:
+                if (state.playerInfo.coconuts < MAX_AMMO) {
+                    item1Value = 1;
+                }
+                break;
+        }
+        if (item1Value > item0Value) {
+            return item1;
+        }
+        else {
+            return item0;
+        }
     }
 }
