c46eb0924f67b857436292cfeb90a996afdc18bc
JoBuehle <jobuehle@ksr.ch>
2022-01-19 16:12:59 +0100

    added projectile in css (no animation)

--- server/static/view.js
+++ server/static/view.js
@@ -42,9 +42,10 @@ class View{
             }
             row+=1;
         }
+        this.shoot("0", "right");
         //for the shoot animation if players shoot coconut
         for(let player in json.projectiles){
-            this.shoot(player, json.projectiles.direction) //fix, not right yet: do tis<-------------------------------------------------------------------------------------------------------------------------
+            this.shoot(player, json.projectiles.direction); //fix, not right yet: do tis<-------------------------------------------------------------------------------------------------------------------------
         }
 
 
@@ -57,9 +58,9 @@ class View{
         }
     }
 
-    shoot(uuid, direction){ //creates an projectile element with direction property where the player is.
+    shoot(id, direction){ //creates an projectile element with direction property where the player is.
 
-        var player = document.getElementById(uuid);
+        var player = document.getElementById(id);
         var projectile = document.createElement("projectile", {direction: direction});
         player.appendChild(projectile);
         
