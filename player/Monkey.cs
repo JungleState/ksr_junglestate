68f16b4d0fa9ebf7ffb6190eb88669b7fe9740ae
PhilippDoenni <philipp.doenni@gmail.com>
2022-01-25 13:17:48 +0100

    sees loot further and probably some other stuff idk

--- player/Monkey.cs
+++ player/Monkey.cs
@@ -8,7 +8,7 @@ public class Monkey : BaseMonkey {
 
     public override Move nextMove(GameState state) {
         // Attack player if possible
-        if (state.playerInfo.coconuts > 0) {
+        if (state.playerInfo.coconuts > 0 && state.playerInfo.lives > 2) {
             Direction target = directionOfEnemyInRangeWithLowestHealth(state);
             if (target != Direction.NONE) {
                 return new Move(Action.THROW, target, state.round, "RATATATATA!");
@@ -22,16 +22,19 @@ public class Monkey : BaseMonkey {
         }
 
         // Otherwise: attempt to move in the same direction as last round (80%)
-        Random random =new System.Random();
+        Random random = new System.Random();
         Random r = new Random();
         if (lastDir.isMoveable() && state.getCell(lastDir).isFree() && 4 > r.Next(5)) {
-            return new Move(Action.MOVE, lastDir, state.round, "Walking is fun.");
+            return new Move(Action.MOVE, lastDir, state.round, "De Matt het min code klaut");
         }
 
         // Otherwise: random move (20%)
         Direction direction = selectRandomDirection(state);
+        if (direction == lastDir.opposite()) {
+            direction = selectRandomDirection(state);
+        }
         lastDir = direction;
-        return new Move(Action.MOVE, direction, state.round, "Where should I go?");
+        return new Move(Action.MOVE, direction, state.round, "ERROR 404: Direction not found");
     }
 
     private Direction selectRandomDirection(GameState state) {
@@ -68,55 +71,91 @@ public class Monkey : BaseMonkey {
 
     private Direction directionOfClosestItemOfHighestValue(GameState state) {
         // returns direction of collectable item that is used most
-        Direction target = Direction.NONE;
         // item can be reached in 1 turn
         foreach (Direction dir in Enum.GetValues(typeof(Direction))) {
             if (dir.isMoveable() && state.getCell(dir).item == Item.PINEAPPLE) {
                 return dir;
             }
-            if (dir.isMoveable() && state.getCell(dir).item == Item.BANANA && state.playerInfo.lives < MAX_LIVES) {
+            if (dir.isMoveable() && state.getCell(dir).item == Item.BANANA) {
                 return dir;
             }
             if (dir.isMoveable() && state.getCell(dir).item == Item.COCONUT && state.playerInfo.coconuts < MAX_AMMO) {
                 return dir;
             }
         }
-        // item can be reached in 2 turns
-        List<Direction> direction2List = new List<Direction>();
-
-
-
-
-        return target;
+        // item can be reached in multiple turns
+        (int, int) coordinatesOfBestItem = (404, 404);
+        for (int i = 0; i < state.cells.Length; i++) {
+            for (int j = 0; j < state.cells[i].Length; j++) {
+                if (state.cells[i][j].isFree() && state.cells[i][j].item != Item.EMPTY) {
+                    coordinatesOfBestItem = compareTwoItems(state, coordinatesOfBestItem, (i, j));
+                }
+            }
+        }
+        // test if any found
+        if (coordinatesOfBestItem == (404, 404)) {
+            return Direction.NONE;
+        }
+        // move to item
+        Random random = new System.Random();
+        Random r = new Random();
+        if (r.Next(2) == 0) {
+            if (coordinatesOfBestItem.Item1 < 2 && state.cells[1][2].isFree()) {
+                return Direction.UP;
+            }
+            if (coordinatesOfBestItem.Item1 > 2 && state.cells[3][2].isFree()) {
+                return Direction.DOWN;
+            }
+            if (coordinatesOfBestItem.Item2 < 2 && state.cells[2][1].isFree()) {
+                return Direction.LEFT;
+            }
+            if (coordinatesOfBestItem.Item2 > 2 && state.cells[2][3].isFree()) {
+                return Direction.RIGHT;
+            }
+        }
+        else {
+            if (coordinatesOfBestItem.Item2 < 2 && state.cells[2][1].isFree()) {
+                return Direction.LEFT;
+            }
+            if (coordinatesOfBestItem.Item2 > 2 && state.cells[2][3].isFree()) {
+                return Direction.RIGHT;
+            }
+            if (coordinatesOfBestItem.Item1 < 2 && state.cells[1][2].isFree()) {
+                return Direction.UP;
+            }
+            if (coordinatesOfBestItem.Item1 > 2 && state.cells[3][2].isFree()) {
+                return Direction.DOWN;
+            }
+        }
+        
+        return Direction.NONE;
     }
 
-    private Item compareTwoItems(GameState state, Item item0, Item item1) {
+    private (int, int) compareTwoItems(GameState state, (int, int) coordinates0, (int, int) coordinates1) {
         // returns the item that is needed most
         int item0Value = 0;
         int item1Value = 0;
-        switch(item0) {
-            case Item.PINEAPPLE:
-                item0Value = 3;
-                break;
-            case Item.BANANA:
-                if (state.playerInfo.lives < MAX_LIVES) {
+        if (coordinates0 != (404, 404)) {
+            switch(state.cells[coordinates0.Item1][coordinates0.Item2].item) {
+                case Item.PINEAPPLE:
+                    item0Value = 3;
+                    break;
+                case Item.BANANA:
                     item0Value = 2;
-                }
-                break;
-            case Item.COCONUT:
-                if (state.playerInfo.coconuts < MAX_AMMO) {
-                    item0Value = 1;
-                }
-                break;
-        }
-        switch(item1) {
+                    break;
+                case Item.COCONUT:
+                    if (state.playerInfo.coconuts < MAX_AMMO) {
+                        item0Value = 1;
+                    }
+                    break;
+            }
+        }        
+        switch(state.cells[coordinates1.Item1][coordinates1.Item2].item) {
             case Item.PINEAPPLE:
                 item1Value = 3;
                 break;
             case Item.BANANA:
-                if (state.playerInfo.lives < MAX_LIVES) {
-                    item1Value = 2;
-                }
+                item1Value = 2;
                 break;
             case Item.COCONUT:
                 if (state.playerInfo.coconuts < MAX_AMMO) {
@@ -125,10 +164,10 @@ public class Monkey : BaseMonkey {
                 break;
         }
         if (item1Value > item0Value) {
-            return item1;
+            return coordinates1;
         }
         else {
-            return item0;
+            return coordinates0;
         }
     }
 }
